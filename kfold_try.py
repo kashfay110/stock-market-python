2215d97a832ee007b0e3010948d36174e1cb815b
kashfay110 <67712224+kashfay110@users.noreply.github.com>
2021-04-09 01:29:29 +0100

    implementing kfold/ add kfold for LR and put in svr_fb.py

--- kfold_try.py
+++ kfold_try.py
@@ -2,9 +2,10 @@ import quandl
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.svm import SVR
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split, cross_val_score, KFold
 import pandas as pd
 from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_absolute_percentage_error
+
 import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
 import mplfinance as mpf
@@ -55,15 +56,16 @@ print(z)
 # test_set = x_test + y_test
 
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
+cv_kfold = KFold(n_splits=10, random_state=1, shuffle=True)
 
 # Create and train the Support Vector Machine (Regressor)
 svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
 svr_rbf.fit(x_train, y_train)
-train_scores = cross_val_score(svr_rbf, x_train, y_train, scoring='accuracy',cv=5)
-test_scores = cross_val_score(svr_rbf, x_test, y_test, scoring='accuracy',cv=5)
+train_scores = cross_val_score(svr_rbf, x_train, y_train,cv=cv_kfold)
+test_scores = cross_val_score(svr_rbf, x_test, y_test,cv=cv_kfold)
 
-
+print(train_scores)
+print(test_scores)
 
 #Set x_forecast equal to the last forecast_out rows of the original dat set from Adj. Close column
 x_forecast = np.array(df.drop(['Prediction'],1))[-forecast_out:]
@@ -73,24 +75,14 @@ x_forecast = np.array(df.drop(['Prediction'],1))[-forecast_out:]
 svm_prediction = svr_rbf.predict(x_forecast)
 print(f'The next {forecast_out} days by the SVR model: \n{svm_prediction}')
 
-
-
-
 # Testing Model: Score returns the coefficient of determination R^2 of the prediction
 # Best possible score is 1.0
 svm_confidence = svr_rbf.score(x_test, y_test)
 print("svr confidence: ", svm_confidence)
 
-
-
 ## Creating predicted values for both models
 y_pred_svr = svr_rbf.predict(x_test)
-
 
 # Mean Squared Error for SVR
 mse_svr = mean_squared_error(y_test, y_pred_svr)
 print(f'The MSE for the SVR algorithm is: {mse_svr}')
-
-
-
-
